7478598c70f84200e27d76a2e56d0e5ed67ee9f3
Yanhua Zhang <yanhua.dev@gmail.com>
2015-01-13 13:43:30 +0800

    try catch test.

---
+++ try_catch_final_test/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace try_catch_final_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            tryfinalTest();
+        }
+
+        static void tryfinalTest()
+        {
+            try
+            {
+                Console.WriteLine("tryfinalTest - try called.");
+                return;
+            }
+            finally
+            {
+                Console.WriteLine("tryfinalTest - finally called.");
+            }
+        }
+    }
+}
