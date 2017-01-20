a1e50d69be6794b4793b06292793fdee70286b90
Yanhua ZHANG <yzhang31@slb.com>
2017-01-20 18:13:47 +0800

    implement merge sort.

--- cpp_algorithm/sort/sort.cpp
+++ cpp_algorithm/sort/sort.cpp
@@ -183,28 +183,129 @@ void select_sort2(vector<int>& arr)
 }
 
 
+//void insert_sort(vector<int>& arr)
+//{
+//	for (int i = 0; i < arr.size() - 1; i++)
+//	{
+//			int candidate = arr[i + 1];
+//			int j = i;  // start from tail pos.
+//			for (; j >= 0; j--)  
+//			{
+//				if (candidate < arr[j])
+//					arr[j + 1] = arr[j];
+//				else
+//					break;
+//			}
+//			arr[j + 1 ] = candidate;
+//	}
+//}
+
 void insert_sort(vector<int>& arr)
 {
 	for (int i = 0; i < arr.size() - 1; i++)
 	{
-			int candidate = arr[i + 1];
-			int j = i;  // start from tail pos.
-			for (; j >= 0; j--)  
-			{
-				if (candidate < arr[j])
-					arr[j + 1] = arr[j];
-				else
-					break;
-			}
-			arr[j + 1 ] = candidate;
+		int candidate = arr[i + 1];
+		int j = i;  // start from tail pos.
+		for (; j >= 0; j--)
+		{
+			if (candidate < arr[j])
+				arr[j + 1] = arr[j];
+			else
+				break;
+		}
+		arr[j + 1] = candidate;
 	}
 }
+
+void insert_sort2(vector<int>& arr)
+{
+	for (int i = 1; i < arr.size(); i++)
+	{
+		int candidate = arr[i];
+		int j = i - 1;  
+		for (; j >= 0 && candidate < arr[j]; j--)
+		{
+			arr[j + 1] = arr[j];
+		}
+		arr[j + 1] = candidate;
+	}
+}
+
+
+void split(vector<int>& arr, vector<int>& left, vector<int>& right)
+{
+	int mid = arr.size() / 2; 
+	for (size_t i = 0; i < arr.size(); i++)
+	{
+		if (i < mid)
+			left.push_back(arr[i]);
+		else
+			right.push_back(arr[i]);
+	}
+}
+
+vector<int> merge(vector<int>& arr1, vector<int>& arr2)
+{
+	vector<int> result;
+	result.resize(arr1.size() + arr2.size());
+	int i = 0, j = 0;
+
+	for (size_t index = 0; index < arr1.size() + arr2.size(); index++)
+	{
+		if (i > arr1.size() - 1 )
+		{
+			result[index] = arr2[j];
+			j++;
+		}
+		else if (j > arr2.size() - 1)
+		{
+			result[index] = arr1[i];
+			i++;
+
+		}
+		else if (arr1[i] < arr2[j])
+		{
+			result[index] = arr1[i];
+			i++;
+		}
+		else
+		{
+			result[index] = arr2[j];
+			j++;
+		}
+	}
+	return result;
+}
+
+
+vector<int> merge_sort(vector<int>& arr)
+{
+	if (arr.size() <= 1)
+	{
+		return arr;
+	}
+
+	vector<int> left, right;
+
+	split(arr, left, right);
+	
+	left = merge_sort(left);
+
+	right = merge_sort(right);
+
+	vector<int> result = merge(left, right);
+	
+	return result;
+}
+
 
 
 int main()
 {
 	//vector<int> unsorted_array = {2,1,3,4,5,6,0,7,8};
 	vector<int> one_val_array = {5 };
+	vector<int> two_val_array = { 5 , 4};
+	vector<int> three_val_array = { 5, 4, 3 };
 	vector<int> unsorted_array = { 0, 7,8,5,2,1,3,4,6 }; // wrong bubble_sort2_wrong cannot handle.
 	//vector<int> unsorted_array = { 7, 1,8,5,2,0,3,4,6 }; // wrong bubble_sort2_wrong cannot handle.
 
@@ -225,10 +326,12 @@ int main()
 	//bubble_sort2_wrong(vector<int>(unsorted_array));
 
 	//select_sort2(unsorted_array);
-	insert_sort(unsorted_array);
+	//insert_sort2(unsorted_array);
+
+	auto result = merge_sort(unsorted_array);
 
 	std::cout << "sorted" << endl;
-	output_array(unsorted_array);
+	output_array(result);
     return 0;
 }
 
