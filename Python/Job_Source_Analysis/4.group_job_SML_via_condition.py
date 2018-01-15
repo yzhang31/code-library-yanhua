e1f965e0dc7a2d24ea568c42284a216553cbb27f
Yanhua Zhang <yanhua.dev@gmail.com>
2018-01-15 18:18:50 +0800

    generate summary csv with report header info.

--- Python/Job_Source_Analysis/4.group_job_SML_via_condition.py
+++ Python/Job_Source_Analysis/4.group_job_SML_via_condition.py
@@ -6,107 +6,48 @@ import zipfile
 import re
 import csv
 
-def build_job_file_map(source_dir):
-    guid_files_map = {}
-    for root, dirs, files in os.walk(os.path.abspath(source_dir)):
-        for file in files:
-            full_path = os.path.join(root, file)
-            guid = build_job_guid(file)
-            if(guid in guid_files_map):
-                guid_files_map[guid].append(str(full_path))
-            else:
-                guid_files_map[guid] = [str(full_path)]
-    return  guid_files_map
-
 def build_job_guid(file_name):
     seps = str(file_name).split("_")
     return seps[-5]
 
-def generate_single_job_summary(source_file_list):
-    row_dict = {"job_guid":"",
-                "MW_version": "",
-                "well_name": "",
-                "client_name": "",
-                "work_flow": "",
-                "simulator": "",
-                "machine_name": ""}
-    for f in source_file_list:
-        print(f)
-        file = open(f, encoding="utf8")
-        content = file.read()
-        job_guid = find("JobGUID=",";",content)
-        MW_version = find("MWVersion=",r'\n',content)
-        well_name = find("WellName=", r';', content)
-        client_name = find("ClientName=", r';', content)
-        work_flow = find("Workflow=", r';', content)
-        simulator = find("Simulator=", r';', content)
-        machine_name = find("MachineName=", r';', content)
-        file.close()
-        row_dict["job_guid"] = row_dict["job_guid"] if job_guid == "" else job_guid
-        row_dict["MW_version"] = row_dict["MW_version"] if MW_version == "" else MW_version
-        row_dict["well_name"] = row_dict["well_name"] if well_name == "" else well_name
-        row_dict["client_name"] = row_dict["client_name"] if client_name == "" else client_name
-        row_dict["work_flow"] = row_dict["work_flow"] if work_flow == "" else work_flow
-        row_dict["simulator"] = row_dict["simulator"] if simulator == "" else simulator
-        row_dict["machine_name"] = row_dict["machine_name"] if machine_name == "" else machine_name
+def build_valid_job_guid_info(csv_path):
+    job_infos = {}
+    with open(csv_path, 'r', newline='', encoding="utf-8") as csvFile:
+        reader = csv.DictReader(csvFile)
+        for jobinfo in reader:
+            row = {'job_guid': jobinfo['job_guid'],'MW_version':jobinfo['MW_version'], 'work_flow':jobinfo['work_flow'], 'simulator':jobinfo['simulator']}
+            need_copy = check_job_guid_info(row)
+            if(need_copy):
+                job_infos[str(jobinfo['job_guid']).upper()] = row
+    return job_infos
 
-    return row_dict
+def check_job_guid_info(job_info):
+    if(job_info['simulator'] == 'True'):
+        return False
 
-def find(start, end, inputtext):
-    result = []
-    search = r'(?<={}).*?(?={})'.format(start, end)
-    for matchedtext in re.findall(search, inputtext):
-        result.append(matchedtext)
+    taget_versions = ['95333','92053','94233']
+    if any(version in job_info['MW_version'] for version in taget_versions):
+        return  True
 
-    if(len(result) > 0):
-        return result[0]
-    else:
-        return ""
+    return False
 
-def generate_summary_csv(summary):
-    with open(r'd:\names.csv', 'w', newline='',encoding="utf-8") as csvfile:
-        fieldnames = ["job_guid",
-                    "MW_version",
-                    "well_name",
-                    "client_name",
-                    "work_flow",
-                    "simulator",
-                    "machine_name"]
+def group_files_via_workflows(source_path, dest_path, summary):
 
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for row in summary:
-            writer.writerow(row[0])
-    return
+    for root, dirs, files in os.walk(os.path.abspath(source_dir)):
+        for file in files:
+            full_path = os.path.join(root, file)
+            guid = build_job_guid(file)
+            if(guid.upper() in summary):
+                print('copy ... {0}'.format(str(file)))
+                shutil.copy2(full_path, dest_path)
 
+source_dir = r'\\srv004bgc.dir.slb.com\MaxwellShareData\Core\Computation\YZhang31\analytics\user_operations\2'
 
+csv_path = r'd:\4\job_summary_no_empty_line.csv'
 
-def copy_given_version_files(summary, mw_versions):
-    for row in summary:
-        if row[0]['MW_version'] in mw_versions and row[0]['simulator'] == 'False':
-            for path in row[1]:
-                shutil.copy2(path, r'd:\3')
-    return
+info_dict = build_valid_job_guid_info(csv_path)
 
-
-#open csv.
-
+group_files_via_workflows(source_dir, r'd:\5', info_dict)
 #loop files in source dir.
-
-#check job guid in csv and compare condiation: 1. workflow, 2. simulation, 3. maxwell version.
-
-#copy relative files into workflow folders
-
-source_dir = "D:\\2\\"
-#source_dir = r'D:\analytics\2'
-maps = build_job_file_map(source_dir);
-
-summary = []
-for job_guid in maps:
-    row_dict = generate_single_job_summary(maps[job_guid])
-    summary.append((row_dict, maps[job_guid]))
-
-generate_summary_csv(summary)
-copy_given_version_files(summary, ['8.0.95333.3100','8.0.94233.3100','8.0.92053.3100'])
 
 print(str(__file__) + "done")
--- Python/Job_Source_Analysis/5.generate_full_report_summary.py
+++ Python/Job_Source_Analysis/5.generate_full_report_summary.py
@@ -22,33 +22,113 @@ def build_job_guid(file_name):
     seps = str(file_name).split("_")
     return seps[-5]
 
+
+
 def generate_single_job_summary(source_file_list):
     row_dict = {"job_guid":"",
+                "job_name":"",
                 "MW_version": "",
                 "well_name": "",
                 "client_name": "",
                 "work_flow": "",
                 "simulator": "",
-                "machine_name": ""}
+                "machine_name": "",
+                "RTBackupDir": None,
+                "SON":None,
+                "LUN": None,
+                "LUL":None,
+                "LATI":None,
+                "LONG":None}
     for f in source_file_list:
         print(f)
         file = open(f, encoding="utf8")
         content = file.read()
         job_guid = find("JobGUID=",";",content)
+        job_name = find("JobName=",";",content)
         MW_version = find("MWVersion=",r'\n',content)
         well_name = find("WellName=", r';', content)
         client_name = find("ClientName=", r';', content)
         work_flow = find("Workflow=", r';', content)
         simulator = find("Simulator=", r';', content)
         machine_name = find("MachineName=", r';', content)
+        RTBackupDir = find("RTBackupDir=", r';', content)
+        SON = find("OSDD=SON", r'', content)
+        LUN = find("OSDD=LUN", r'', content)
+        LUL = find("OSDD=LUL", r'', content)
+        LATI = find("Enter Longitude,Input,TextBox,", r',UIInteraction', content)
+        LONG = find("Enter Latitude,Input,TextBox", r',UIInteraction', content)
+
         file.close()
-        row_dict["job_guid"] = row_dict["job_guid"] if job_guid == "" else job_guid
-        row_dict["MW_version"] = row_dict["MW_version"] if MW_version == "" else MW_version
-        row_dict["well_name"] = row_dict["well_name"] if well_name == "" else well_name
-        row_dict["client_name"] = row_dict["client_name"] if client_name == "" else client_name
-        row_dict["work_flow"] = row_dict["work_flow"] if work_flow == "" else work_flow
-        row_dict["simulator"] = row_dict["simulator"] if simulator == "" else simulator
-        row_dict["machine_name"] = row_dict["machine_name"] if machine_name == "" else machine_name
+        row_dict["job_guid"] = row_dict["job_guid"] if job_guid == None else job_guid
+        row_dict["job_name"] = row_dict["job_name"] if job_guid == None else job_name
+        row_dict["MW_version"] = row_dict["MW_version"] if MW_version == None else MW_version
+        row_dict["well_name"] = row_dict["well_name"] if well_name == None else well_name
+        row_dict["client_name"] = row_dict["client_name"] if client_name == None else client_name
+        row_dict["work_flow"] = row_dict["work_flow"] if work_flow == None else work_flow
+        row_dict["simulator"] = row_dict["simulator"] if simulator == None else simulator
+        row_dict["machine_name"] = row_dict["machine_name"] if machine_name == None else machine_name
+        row_dict["RTBackupDir"] = row_dict["RTBackupDir"] if RTBackupDir == None else RTBackupDir
+        row_dict["SON"] = row_dict["SON"] if SON == None else SON
+        row_dict["LUN"] = row_dict["LUN"] if LUN == None else LUN
+        row_dict["LUL"] = row_dict["LUL"] if LUL == None else LUL
+        row_dict["LATI"] = row_dict["LATI"] if LATI == None else LATI
+        row_dict["LONG"] = row_dict["LONG"] if LONG == None else LONG
+
+    return row_dict
+
+
+def generate_single_job_summary(source_file_list):
+    row_dict = {"job_guid":"",
+                "job_name":"",
+                "MW_version": "",
+                "well_name": "",
+                "client_name": "",
+                "work_flow": "",
+                "simulator": "",
+                "machine_name": "",
+                "RTBackupDir": None,
+                "SON":None,
+                "LUN": None,
+                "LUL":None,
+                "LATI":None,
+                "LONG":None,
+                "BLI":None}
+    for f in source_file_list:
+        print(f)
+        file = open(f, encoding="utf8")
+        content = file.read()
+        job_guid = find("JobGUID=",";",content)
+        job_name = find("JobName=",";",content)
+        MW_version = find("MWVersion=",r'\n',content)
+        well_name = find("WellName=", r';', content)
+        client_name = find("ClientName=", r';', content)
+        work_flow = find("Workflow=", r';', content)
+        simulator = find("Simulator=", r';', content)
+        machine_name = find("MachineName=", r';', content)
+        RTBackupDir = find("RTBackupDir=", r';', content)
+        SON = find("OSDD=SON", r'', content)
+        LUN = find("OSDD=LUN", r'', content)
+        LUL = find("OSDD=LUL", r'', content)
+        LATI = find("Enter Longitude,Input,TextBox,", r',UIInteraction', content)
+        LONG = find("Enter Latitude,Input,TextBox", r',UIInteraction', content)
+        BLI = find("Enter BLI,Input,TextBox,", r',UIInteraction', content)
+
+        file.close()
+        row_dict["job_guid"] = row_dict["job_guid"] if job_guid == None else job_guid
+        row_dict["job_name"] = row_dict["job_name"] if job_guid == None else job_name
+        row_dict["MW_version"] = row_dict["MW_version"] if MW_version == None else MW_version
+        row_dict["well_name"] = row_dict["well_name"] if well_name == None else well_name
+        row_dict["client_name"] = row_dict["client_name"] if client_name == None else client_name
+        row_dict["work_flow"] = row_dict["work_flow"] if work_flow == None else work_flow
+        row_dict["simulator"] = row_dict["simulator"] if simulator == None else simulator
+        row_dict["machine_name"] = row_dict["machine_name"] if machine_name == None else machine_name
+        row_dict["RTBackupDir"] = row_dict["RTBackupDir"] if RTBackupDir == None else RTBackupDir
+        row_dict["SON"] = row_dict["SON"] if SON == None else SON
+        row_dict["LUN"] = row_dict["LUN"] if LUN == None else LUN
+        row_dict["LUL"] = row_dict["LUL"] if LUL == None else LUL
+        row_dict["LATI"] = row_dict["LATI"] if LATI == None else LATI
+        row_dict["LONG"] = row_dict["LONG"] if LONG == None else LONG
+        row_dict["BLI"] = row_dict["BLI"] if BLI == None else BLI
 
     return row_dict
 
@@ -61,43 +141,41 @@ def find(start, end, inputtext):
     if(len(result) > 0):
         return result[0]
     else:
-        return ""
+        return None
 
 def generate_summary_csv(summary):
-    with open(r'd:\names.csv', 'w', newline='',encoding="utf-8") as csvfile:
+    with open(r'd:\job_info_summary.csv', 'w', newline='',encoding="utf-8") as csvfile:
         fieldnames = ["job_guid",
+                      "job_name",
                     "MW_version",
                     "well_name",
                     "client_name",
                     "work_flow",
                     "simulator",
-                    "machine_name"]
+                    "machine_name",
+                      "RTBackupDir",
+                      "SON",
+                      "LUN",
+                      "LUL",
+                      "LATI",
+                      "LONG",
+                      "BLI"]
 
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for row in summary:
-            writer.writerow(row[0])
+            writer.writerow(row)
     return
 
+source_dir = "D:\\5\\"
 
-
-def copy_given_version_files(summary, mw_versions):
-    for row in summary:
-        if row[0]['MW_version'] in mw_versions and row[0]['simulator'] == 'False':
-            for path in row[1]:
-                shutil.copy2(path, r'd:\3')
-    return
-
-source_dir = "D:\\2\\"
-#source_dir = r'D:\analytics\2'
-maps = build_job_file_map(source_dir);
+guid_files_map = build_job_file_map(source_dir)
 
 summary = []
-for job_guid in maps:
-    row_dict = generate_single_job_summary(maps[job_guid])
-    summary.append((row_dict, maps[job_guid]))
+for job_guid in guid_files_map:
+    row_dict = generate_single_job_summary(guid_files_map[job_guid])
+    summary.append((row_dict))
 
 generate_summary_csv(summary)
-copy_given_version_files(summary, ['8.0.95333.3100','8.0.94233.3100','8.0.92053.3100'])
 
 print(str(__file__) + "done")
